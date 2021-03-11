badfae6b57a42c1e3e0bf0d3ca6da03704eec7a4
Boris <boris@bzkj.tech>
2021-03-12 00:05:57 +0800

    丰富示例

--- tests/air-spider/test_air_spider.py
+++ tests/air-spider/test_air_spider.py
@@ -15,6 +15,12 @@ class TestAirSpider(feapder.AirSpider):
     def start_requests(self, *args, **kws):
         yield feapder.Request("https://www.baidu.com")
 
+    def download_midware(self, request):
+        # request.headers = {'User-Agent': ""}
+        # request.proxies = {"https":"https://12.12.12.12:6666"}
+        # request.cookies = {}
+        return request
+
     def parse(self, request, response):
         print(response.xpath("//title").extract_first())
 
