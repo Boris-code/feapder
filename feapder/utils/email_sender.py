d1034d35a3858cffbf5281f0ac8c05b81b9fe0e6
Boris <boris@bzkj.tech>
2021-03-27 10:16:50 +0800

    修复邮箱报警导入包的bug

--- feapder/utils/email_sender.py
+++ feapder/utils/email_sender.py
@@ -15,7 +15,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formataddr
 
-from utils.log import log
+from feapder.utils.log import log
 
 
 class EmailSender(object):
