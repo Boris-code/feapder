540e329cb71915857443ecdf2c2d9b85a8236d06
Boris <564773807@qq.com>
2021-04-25 12:18:42 +0800

    review mongodb

--- feapder/db/mongodb.py
+++ feapder/db/mongodb.py
@@ -11,31 +11,14 @@ from typing import List, Dict
 from urllib import parse
 
 import pymongo.errors
-from pymongo.collection import Collection
 from pymongo import MongoClient
+from pymongo.collection import Collection
 
 import feapder.setting as setting
 from feapder.utils.log import log
 
 
-def auto_retry(func):
-    def wapper(*args, **kwargs):
-        for i in range(3):
-            try:
-                return func(*args, **kwargs)
-            except Exception as e:
-                log.error(
-                    """
-                    error:%s
-                    sql:  %s
-                    """.format(e, kwargs.get("sql") or args[1])
-                )
-    
-    return wapper
-
-
 class MongoDB:
-    
     def __init__(
         self, ip=None, port=None, db=None, user_name=None, user_pass=None, **kwargs
     ):
@@ -49,20 +32,17 @@ class MongoDB:
             user_name = setting.MONGO_USER_NAME
         if not user_pass:
             user_pass = setting.MONGO_USER_PASS
-        
+
         self.client = MongoClient(
-            host=ip,
-            port=port,
-            username=user_name,
-            password=user_pass
+            host=ip, port=port, username=user_name, password=user_pass
         )
         self.db = self.client.get_database(db)
-    
+
     @classmethod
     def from_url(cls, url, **kwargs):
         # mongodb://username:password@ip:port/db
         url_parsed = parse.urlparse(url)
-        
+
         db_type = url_parsed.scheme.strip()
         if db_type != "mongodb":
             raise Exception(
@@ -70,21 +50,20 @@ class MongoDB:
                     url
                 )
             )
-        
+
         connect_params = {}
         connect_params["ip"] = url_parsed.hostname.strip()
         connect_params["port"] = url_parsed.port
         connect_params["user_name"] = url_parsed.username.strip()
         connect_params["user_pass"] = url_parsed.password.strip()
         connect_params["db"] = url_parsed.path.strip("/").strip()
-        
+
         connect_params.update(kwargs)
         return cls(**connect_params)
-    
+
     def get_collection(self, collection_name) -> Collection:
         return self.db.get_collection(collection_name)
-    
-    @auto_retry
+
     def find(self, table, condition=None, limit=0) -> List[Dict]:
         """
         @summary:
@@ -102,7 +81,7 @@ class MongoDB:
         result = list(cursor.limit(limit))
         cursor.close()
         return result
-    
+
     def add(self, table, data, **kwargs):
         """
 
@@ -119,15 +98,16 @@ class MongoDB:
         Returns: 添加行数
 
         """
-        affect_count = None
-        auto_update = kwargs.get('auto_update', False)
-        update_columns = kwargs.get('update_columns', ())
-        insert_ignore = kwargs.get('insert_ignore', False)
-        condition_fields = kwargs.get('condition_fields', ['_id'])
-        exception_callfunc = kwargs.get('exception_callfunc', None)
+        affect_count = 1
+        auto_update = kwargs.get("auto_update", False)
+        update_columns = kwargs.get("update_columns", ())
+        insert_ignore = kwargs.get("insert_ignore", False)
+        condition_fields = kwargs.get("condition_fields", ["_id"])
+        exception_callfunc = kwargs.get("exception_callfunc", None)
         try:
             collection = self.get_collection(table)
-            
+
+            # 存在则更新
             if update_columns:
                 if not isinstance(update_columns, (tuple, list)):
                     update_columns = [update_columns]
@@ -135,35 +115,40 @@ class MongoDB:
                 try:
                     collection.insert_one(data)
                 except pymongo.errors.DuplicateKeyError:
-                    # 重复时不忽略
-                    if not insert_ignore:
-                        condition = {condition_field: data[condition_field] for condition_field in condition_fields}
-                        doc = {key: data[key] for key in update_columns}
-                        collection.update_one(condition, {'$set': doc})
-                affect_count = 1
-            
+                    condition = {
+                        condition_field: data[condition_field]
+                        for condition_field in condition_fields
+                    }
+                    doc = {key: data[key] for key in update_columns}
+                    collection.update_one(condition, {"$set": doc})
+
+            # 覆盖更新
             elif auto_update:
-                condition = {condition_field: data[condition_field] for condition_field in condition_fields}
+                condition = {
+                    condition_field: data[condition_field]
+                    for condition_field in condition_fields
+                }
                 # 替换已存在的数据
                 collection.replace_one(condition, data)
-                affect_count = 1
 
             else:
                 try:
                     collection.insert_one(data)
-                    affect_count = 1
-                except pymongo.errors.DuplicateKeyError:
-                    affect_count = 0
-                    
+                except pymongo.errors.DuplicateKeyError as e:
+                    if not insert_ignore:
+                        raise e
+                    else:
+                        affect_count = 0
+
         except Exception as e:
-            log.error(
-                "error:{}".format(e)
-            )
+            log.error("error: {}".format(e))
             if exception_callfunc:
                 exception_callfunc(e)
-        
+
+            affect_count = 0
+
         return affect_count
-    
+
     def add_smart(self, table, data: Dict, **kwargs):
         """
         添加数据, 直接传递json格式的数据，不用拼sql
@@ -176,7 +161,7 @@ class MongoDB:
 
         """
         return self.add(table, data, **kwargs)
-    
+
     def add_batch(self, table: str, datas: List[Dict], **kwargs):
         """
         @summary: 批量添加数据
@@ -192,55 +177,61 @@ class MongoDB:
         @result: 添加行数
         """
         affect_count = None
-        auto_update = kwargs.get('auto_update', False)
-        update_columns = kwargs.get('update_columns', ())
-        update_columns_value = kwargs.get('update_columns_value', ())
-        condition_fields = kwargs.get('condition_fields', ['_id'])
-        
+        auto_update = kwargs.get("auto_update", False)
+        update_columns = kwargs.get("update_columns", ())
+        update_columns_value = kwargs.get("update_columns_value", ())
+        condition_fields = kwargs.get("condition_fields", ["_id"])
+
         try:
             collection = self.get_collection(table)
             affect_count = 0
-            
+
             if update_columns:
                 if not isinstance(update_columns, (tuple, list)):
                     update_columns = [update_columns]
-                
+
                 for data in datas:
                     try:
                         collection.insert_one(data)
                     except pymongo.errors.DuplicateKeyError:
                         # 数据冲突，只更新指定字段
-                        condition = {condition_field: data[condition_field] for condition_field in condition_fields}
+                        condition = {
+                            condition_field: data[condition_field]
+                            for condition_field in condition_fields
+                        }
                         doc = {
                             key: value
                             for key, value in zip(update_columns, update_columns_value)
                         }
-                        collection.update_one(condition, {'$set': doc})
+                        collection.update_one(condition, {"$set": doc})
                     affect_count += 1
-            
+
             elif auto_update:
                 for data in datas:
-                    condition = {condition_field: data[condition_field] for condition_field in condition_fields}
+                    condition = {
+                        condition_field: data[condition_field]
+                        for condition_field in condition_fields
+                    }
                     # 如果找到就替换，否则插入
-                    result = collection.find_one_and_replace(condition, data, upsert=True)
+                    result = collection.find_one_and_replace(
+                        condition, data, upsert=True
+                    )
                     affect_count += 1
-            
+
             else:
                 for data in datas:
                     try:
-                        collection.insert_one(data)
+                        collection.insert_one(data)  # TODO 实现真正的批量插入
                     except pymongo.errors.DuplicateKeyError:
                         # 忽略冲突
                         continue
                     affect_count += 1
 
         except Exception as e:
-            log.error(
-                "error:{}".format(e)
-            )
-        
+            log.error("error:{}".format(e))
+
         return affect_count
-    
+
     def add_batch_smart(self, table, datas: List[Dict], **kwargs):
         """
         批量添加数据, 直接传递list格式的数据，不用拼sql
@@ -258,22 +249,24 @@ class MongoDB:
         if not datas:
             return
         return self.add_batch(table, datas, **kwargs)
-    
+
     def update(self, table, data: Dict, condition: Dict):
         try:
             collection = self.get_collection(table)
-            collection.update_one(condition, {'$set': data})
+            collection.update_one(condition, {"$set": data})
         except Exception as e:
             log.error(
                 """
                 error:{}
                 condition: {}
-            """.format(e, condition)
+            """.format(
+                    e, condition
+                )
             )
             return False
         else:
             return True
-    
+
     def update_smart(self, table, data: Dict, condition: Dict):
         """
         更新
@@ -285,7 +278,7 @@ class MongoDB:
         Returns: True / False
         """
         return self.update(table, data, condition)
-    
+
     def delete(self, table, condition: Dict):
         """
         删除
@@ -303,7 +296,9 @@ class MongoDB:
                 """
                 error:{}
                 condition: {}
-            """.format(e, condition)
+            """.format(
+                    e, condition
+                )
             )
             return False
         else:
