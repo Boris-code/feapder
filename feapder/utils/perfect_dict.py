f8fb0df9dc17c63d2664e36232f327f14b1dae30
Boris <564773807@qq.com>
2021-08-11 12:42:18 +0800

    完美的字典封装

--- feapder/utils/perfect_dict.py
+++ feapder/utils/perfect_dict.py
@@ -11,6 +11,9 @@ Created on 2021/4/8 11:32 上午
 
 class PerfectDict(dict):
     """
+    >>> data = PerfectDict({"id":1, "url":"xxx"})
+    >>> data
+    {'id': 1, 'url': 'xxx'}
     >>> data = PerfectDict(id=1, url="xxx")
     >>> data
     {'id': 1, 'url': 'xxx'}
@@ -29,9 +32,13 @@ class PerfectDict(dict):
     1
     >>> data[1]
     'xxx'
-    >>> data = PerfectDict({"id":1, "url":"xxx"})
-    >>> data
-    {'id': 1, 'url': 'xxx'}
+    >>> data = PerfectDict({"a": 1, "b": {"b1": 2}})
+    >>> data.b.b1
+    2
+    >>> data[1].b1
+    2
+    >>> data.get("b").b1
+    2
     """
 
     def __init__(self, _dict: dict = None, _values: list = None, **kwargs):
@@ -41,10 +48,34 @@ class PerfectDict(dict):
 
     def __getitem__(self, key):
         if isinstance(key, int):
-            return self.__values__[key]
+            value = self.__values__[key]
         else:
-            return self.__dict__[key]
+            value = self.__dict__[key]
+
+        if isinstance(value, dict):
+            return PerfectDict(value)
+        else:
+            return value
 
     def __iter__(self, *args, **kwargs):
         for value in self.__values__:
-            yield value
+            if isinstance(value, dict):
+                yield PerfectDict(value)
+            else:
+                yield value
+
+    def __getattribute__(self, item):
+        value = object.__getattribute__(self, item)
+        if item == "__dict__":
+            return value
+        elif isinstance(value, dict):
+            return PerfectDict(value)
+        else:
+            return value
+
+    def get(self, key):
+        value = self.__dict__[key]
+        if isinstance(value, dict):
+            return PerfectDict(value)
+        else:
+            return value
