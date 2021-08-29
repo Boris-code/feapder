9bffd5e29518a8e1b7ffb79b476297150a45eb4e
Boris <564773807@qq.com>
2021-08-29 15:12:15 +0800

    修改main模板

--- feapder/templates/project_template/main.py
+++ feapder/templates/project_template/main.py
@@ -11,10 +11,16 @@ from feapder import ArgumentParser
 
 from spiders import *
 
+def crawl_xxx():
+    """
+    AirSpider爬虫
+    """
+    spider = xxx.XXXSpider()
+    spider.start()
 
 def crawl_xxx():
     """
-    普通爬虫
+    Spider爬虫
     """
     spider = xxx.XXXSpider(redis_key="xxx:xxx")
     spider.start()
@@ -22,8 +28,7 @@ def crawl_xxx():
 
 def crawl_xxx(args):
     """
-    批次爬虫
-    @param args: 1 / 2 / init
+    BatchSpider爬虫
     """
     spider = xxx_spider.XXXSpider(
         task_table="",  # mysql中的任务表
@@ -39,7 +44,7 @@ def crawl_xxx(args):
         spider.start_monitor_task()
     elif args == 2:
         spider.start()
-    elif args == "init":
+    elif args == 3:
         spider.init_task()
 
 
@@ -47,10 +52,28 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="xxx爬虫")
 
     parser.add_argument(
-        "--crawl_xxx", action="store_true", help="xxx", function=crawl_xxx
+        "--crawl_xxx", action="store_true", help="xxx爬虫", function=crawl_xxx
     )
     parser.add_argument(
-        "--crawl_xxx", type=int, nargs=1, help="xxx(1|2）", function=crawl_xxx
+        "--crawl_xxx", action="store_true", help="xxx爬虫", function=crawl_xxx
+    )
+    parser.add_argument(
+        "--crawl_xxx",
+        type=int,
+        nargs=1,
+        help="xxx爬虫",
+        choices=[1, 2, 3],
+        function=crawl_xxx,
     )
 
     parser.start()
+
+    # main.py作为爬虫启动的统一入口，提供命令行的方式启动多个爬虫，若只有一个爬虫，可不编写main.py
+    # 将上面的xxx修改为自己实际的爬虫名
+    # 查看运行命令 python main.py --help
+    # AirSpider与Spider爬虫运行方式 python main.py --crawl_xxx
+    # BatchSpider运行方式
+    # 1. 下发任务：python main.py --crawl_xxx 1
+    # 2. 采集：python main.py --crawl_xxx 2
+    # 3. 重置任务：python main.py --crawl_xxx 3
+
