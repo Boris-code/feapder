70507b720d86ebd7eddb459961f7364e5ba99e60
Boris <564773807@qq.com>
2022-04-16 18:17:40 +0800

    添加重试和超时的装饰器

--- feapder/utils/tools.py
+++ feapder/utils/tools.py
@@ -21,6 +21,7 @@ import os
 import pickle
 import random
 import re
+import signal
 import socket
 import ssl
 import string
@@ -133,6 +134,99 @@ def memoizemethod_noargs(method):
         return cache[self]
 
     return new_method
+
+
+def retry(retry_times=3, interval=0):
+    """
+    普通函数的重试装饰器
+    Args:
+        retry_times: 重试次数
+        interval: 每次重试之间的间隔
+
+    Returns:
+
+    """
+
+    def _retry(func):
+        @functools.wraps(func)  # 将函数的原来属性付给新函数
+        def wapper(*args, **kwargs):
+            for i in range(retry_times):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    log.error(
+                        "函数 {} 执行失败 重试 {} 次. error {}".format(func.__name__, i + 1, e)
+                    )
+                    time.sleep(interval)
+                    if i + 1 >= retry_times:
+                        raise e
+
+        return wapper
+
+    return _retry
+
+
+def retry_asyncio(retry_times=3, interval=0):
+    """
+    协程的重试装饰器
+    Args:
+        retry_times: 重试次数
+        interval: 每次重试之间的间隔
+
+    Returns:
+
+    """
+
+    def _retry(func):
+        @functools.wraps(func)  # 将函数的原来属性付给新函数
+        async def wapper(*args, **kwargs):
+            for i in range(retry_times):
+                try:
+                    return await func(*args, **kwargs)
+                except Exception as e:
+                    log.error(
+                        "函数 {} 执行失败 重试 {} 次. error {}".format(func.__name__, i + 1, e)
+                    )
+                    await asyncio.sleep(interval)
+                    if i + 1 >= retry_times:
+                        raise e
+
+        return wapper
+
+    return _retry
+
+
+def func_timeout(timeout):
+    """
+    函数运行时间限制装饰器
+    注: 不支持window
+    Args:
+        timeout: 超时的时间
+
+    Eg:
+        @set_timeout(3)
+        def test():
+            ...
+
+    Returns:
+
+    """
+    def wapper(func):
+        def handle(
+            signum, frame
+        ):  # 收到信号 SIGALRM 后的回调函数，第一个参数是信号的数字，第二个参数是the interrupted stack frame.
+            raise TimeoutError
+
+        def new_method(*args, **kwargs):
+            signal.signal(signal.SIGALRM, handle)  # 设置信号和回调函数
+            signal.alarm(timeout)  # 设置 timeout 秒的闹钟
+            r = func(*args, **kwargs)
+            signal.alarm(0)  # 关闭闹钟
+            return r
+
+        return new_method
+
+    return wapper
 
 
 ########################【网页解析相关】###############################
