bb8632070b6f5b4f00972284c05ba647fba008e8
Boris <564773807@qq.com>
2022-12-05 10:01:51 +0800

    删除配置文件多余的逗号

--- feapder/setting.py
+++ feapder/setting.py
@@ -144,7 +144,7 @@ USE_SESSION = False
 DOWNLOADER = "feapder.network.downloader.RequestsDownloader"
 SESSION_DOWNLOADER = "feapder.network.downloader.RequestsSessionDownloader"
 RENDER_DOWNLOADER = "feapder.network.downloader.SeleniumDownloader"
-# RENDER_DOWNLOADER="feapder.network.downloader.PlaywrightDownloader",
+# RENDER_DOWNLOADER="feapder.network.downloader.PlaywrightDownloader"
 MAKE_ABSOLUTE_LINKS = True  # 自动转成绝对连接
 
 # 去重
