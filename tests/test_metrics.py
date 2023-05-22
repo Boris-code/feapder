b70dd874c7dc0bb36594ce1426517c2c5984f9df
Boris <564773807@qq.com>
2023-05-22 11:51:58 +0800

    test metrics

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from feapder.utils import metrics
 
 # 初始化打点系统
@@ -13,9 +15,38 @@ metrics.init(
 )
 
 
-for i in range(1000):
-    metrics.emit_counter("total count", count=1000, classify="test5")
-    for j in range(1000):
-        metrics.emit_counter("key", count=1, classify="test5")
+async def test_counter_async():
+    for i in range(100):
+        await metrics.aemit_counter("total count", count=100, classify="test5")
+        for j in range(100):
+            await metrics.aemit_counter("key", count=1, classify="test5")
 
-metrics.close()
+
+def test_counter():
+    for i in range(100):
+        metrics.emit_counter("total count", count=100, classify="test5")
+        for j in range(100):
+            metrics.emit_counter("key", count=1, classify="test5")
+
+
+def test_store():
+    metrics.emit_store("total", 100, classify="cookie_count")
+
+
+def test_time():
+    metrics.emit_timer("total", 100, classify="time")
+
+
+def test_any():
+    metrics.emit_any(
+        tags={"_key": "total", "_type": "any"}, fields={"_value": 100}, classify="time"
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(test_counter_async())
+    test_counter_async()
+    test_store()
+    test_time()
+    test_any()
+    metrics.close()
