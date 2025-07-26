aa399d2796bab454485ad848bba803d18543c93d
keepmoving <471293694@qq.com>
2025-07-27 03:33:01 +0800

    MysqlDB 增加 set_session 参数，解决时间默认 UTC 的问题

--- tests/test_mysqldb.py
+++ tests/test_mysqldb.py
@@ -2,7 +2,10 @@ from feapder.db.mysqldb import MysqlDB
 
 
 db = MysqlDB(
-    ip="localhost", port=3306, db="feapder", user_name="feapder", user_pass="feapder123"
+    ip="localhost", port=3306, db="feapder", user_name="feapder", user_pass="feapder123", set_session=["SET time_zone='+08:00'"]
 )
 
 MysqlDB.from_url("mysql://feapder:feapder123@localhost:3306/feapder?charset=utf8mb4")
+
+result = db.find("SELECT @@global.time_zone, @@session.time_zone, date_format(NOW(), '%Y-%m-%d %H:%i:%s')")
+print(f"Database timezone info: {result}")
